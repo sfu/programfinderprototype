e8ad5f9f0543241d1e503963709f40c564206a06
Graham Ballantyne <grahamb@sfu.ca>
2012-08-21 13:32:01 -0700

    hackery to set the first options on select2 & 3

--- prototype/js/select-builder.js
+++ prototype/js/select-builder.js
@@ -3,9 +3,9 @@
 $('#selectLevel1').change(function(ev) {
     var programs = JSON.parse(document.getElementById('programsJson').innerHTML)
     ,   val = programs[$(this).val()]
-    ,   html = ['<option value=""></option>'];
+    ,   html = ['<option value="">' + $('#selectLevel2').children().first().text() + '</option>'];
     if (val) {
-        $('#selectLevel3').html(html.join('')).hide();//attr('disabled', true);
+        $('#selectLevel3').hide();
         $.each(val, function(i, item) {
             if (Object.prototype.toString.call(item) !== '[object Object]') {
                 html.push('<option value="' + item + '">' + item + '</option>');
@@ -14,9 +14,9 @@ $('#selectLevel1').change(function(ev) {
                     html.push('<option data-i="' + i + '" value="' + ii + '">' + ii + '</option>');                }
             }
         });
-        $('#selectLevel2').html(html.join('')).show();//attr('disabled', false);
+        $('#selectLevel2').html(html.join('')).show();
     } else {
-        $('#selectLevel2, #selectLevel3').html(html.join('')).hide();//attr('disabled', true);
+        $('#selectLevel2, #selectLevel3').hide();
     }
 
 });
@@ -27,14 +27,14 @@ $('#selectLevel2').change(function(ev) {
     ,   topLevel = $('#selectLevel1').val()
     ,   idx = el.find(':selected').data('i')
     ,   val = el.val()
-    ,   html = ['<option value=""></option>'];
+    ,   html = ['<option value="">' + $('#selectLevel3').children().first().text() + '</option>'];
     if (idx >= 0) {
         var data = programs[topLevel][idx][val];
         for (var x = 0; x < data.length; x++) {
             html.push('<option value="' + data[x] + '">' + data[x] + '</option>');
         }
-        $('#selectLevel3').html(html.join('')).show();//attr('disabled', false);
+        $('#selectLevel3').html(html.join('')).show();
     } else {
-        $('#selectLevel3').html(html.join('')).hide();//attr('disabled', true);
+        $('#selectLevel3').hide();
     }
 });
